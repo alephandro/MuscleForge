98812094f6b10b517e57af0f0e02704dfc55da3f
GuilledlC <guillermodelacalhacar@gmail.com>
2024-04-16 18:15:50 +0200

    Fix some errors in the exercises INSERT

--- Server/src/database.sql
+++ Server/src/database.sql
@@ -2,28 +2,29 @@
 # mysql -u root MuscleForge < backup.sql
 # mysql -u root < backup.sql
 
+DROP SCHEMA IF EXISTS MuscleForge;
 CREATE SCHEMA MuscleForge;
 
 USE MuscleForge;
 
 CREATE TABLE users (
-	email varchar(100) PRIMARY KEY,
-	password varchar(100)
+					   email varchar(100) PRIMARY KEY,
+					   password varchar(100)
 );
 
 CREATE TABLE exercises (
-	name varchar(50) PRIMARY KEY,
-	muscle_group varchar(50),
-	description varchar(300)
+						   name varchar(50) PRIMARY KEY,
+						   muscle_group varchar(50),
+						   description varchar(300)
 );
-CREATE UNIQUE INDEX idx_muscle_group ON exercises (muscle_group);
+CREATE INDEX idx_muscle_group ON exercises (muscle_group);
 
 INSERT INTO exercises
 VALUES
 
-    ("Bench Press", "Chest", "Lying face up on a bench, lower the barbell to your chest and push it up. Works the chest."),
-	("Incline Bench Press", "Like the Bench Press, but with an incline. Works the upper chest."),
-	("Decline Bench Press", "Like the Bench Press, but with a decline. Works the lower chest."),
+	("Bench Press", "Chest", "Lying face up on a bench, lower the barbell to your chest and push it up. Works the chest."),
+	("Incline Bench Press", "Chest", "Like the Bench Press, but with an incline. Works the upper chest."),
+	("Decline Bench Press", "Chest", "Like the Bench Press, but with a decline. Works the lower chest."),
 	("Push Up", "Chest", "Lie face down on the ground and push yourself up with your arms. Works the chest."),
 	("Dumbbell Fly", "Chest", "Lying face up on a bench, hold the dumbbells in a cross position and bring them together in a hug-like motion. Works the chest."),
 	("Dumbbell Bench Press", "Chest", "Like the Bench Press, but with dumbbels. Works the chest."),
@@ -31,7 +32,7 @@ VALUES
 	("Decline Dumbbell Bench Press", "Chest", "Like the Dumbbell Bench Press, but with a decline. Works the lower chest."),
 	("Weighted Dip", "Chest", "In parallel bars or rings, lower yourself down and push yourself up. Works the chest."),
 
-    ("Back Squat", "Legs", "With a barbell on your back, squat down and get up. Works the quads."),
+	("Back Squat", "Legs", "With a barbell on your back, squat down and get up. Works the quads."),
 	("Front Squat", "Legs", "With a barbell on your clavicles, squat down and get up. Works the quads."),
 	("Hack Squat", "Legs", "Using a Hack Squat machine, squat down and get up. Works the quads."),
 	("Leg Press", "Legs", "Using a Leg Press machine, press the weight with your legs. Works the quads."),
@@ -53,15 +54,15 @@ VALUES
 	("Pull Up", "Back", "Using a pronated grip, pull yourself up toward the bar. Works the upper back and lats."),
 	("Chin Up", "Back", "Using a supinated grip, pull yourself up toward the bar. Works the upper back, lats, and biceps."),
 
-    ("SkullCrusher", "Triceps", "Lying face up on a bench, hold a bar over your head. Bend your elbows and push the weight up again. Works the triceps."),
-    ("French Press", "Triceps", "Sitting up on a bench, hold a bar over your head. Bend your elbows and push the weight up again. Works the triceps."),
-    ("Triceps Dip", "Triceps", "In parallel bars or rings, lower yourself down and push yourself up. Works the triceps."),
-    ("Triceps Kickback", "Triceps", "Supported by a bench, kick the dumbbell or cable back. Works the triceps."),
+	("SkullCrusher", "Triceps", "Lying face up on a bench, hold a bar over your head. Bend your elbows and push the weight up again. Works the triceps."),
+	("French Press", "Triceps", "Sitting up on a bench, hold a bar over your head. Bend your elbows and push the weight up again. Works the triceps."),
+	("Triceps Dip", "Triceps", "In parallel bars or rings, lower yourself down and push yourself up. Works the triceps."),
+	("Triceps Kickback", "Triceps", "Supported by a bench, kick the dumbbell or cable back. Works the triceps."),
 	("Triceps Extension", "Triceps", "Using a cable machine, hold the bar over your head and extend your triceps upwards. Works the triceps."),
 	("Triceps Pushdown", "Triceps", "Using a cable machine, hold the bar in front of you and extend your triceps downwards. Works the triceps."),
 
-    ("Barbell Curl", "Biceps", "Gripping a barbell with a supinated grip, curl your arms. Works the biceps."),
-    ("EZ Bar Curl", "Biceps", "Gripping an EZ bar with a supinated grip, curl your arms. Works the biceps."),
+	("Barbell Curl", "Biceps", "Gripping a barbell with a supinated grip, curl your arms. Works the biceps."),
+	("EZ Bar Curl", "Biceps", "Gripping an EZ bar with a supinated grip, curl your arms. Works the biceps."),
 	("Dumbbell Curl", "Biceps", "Gripping dumbbels in your hands, curl your arms. Works the biceps."),
 	("Cable Curl", "Biceps", "Gripping a cable bar with a supinated grip, curl your arms. Works the biceps."),
 	("Reverse Barbell Curl", "Biceps", "Gripping a barbell with a pronated grip, curl your arms. Works the brachialis and brachioradialis."),
@@ -71,10 +72,10 @@ VALUES
 	("Preacher Curl", "Biceps", "Using a preacher bench, hold a barbell or EZ bar and curl it. Works the biceps."),
 	("Hammer Curl", "Biceps", "Gropping dumbbells with a neutral grip, curl your arms. Works the biceps, brachialis, and brachioradialis"),
 
-    ("Military Press", "Shoulders", "With a barbell on your clavicles, push it up above your head with your arms. Works the front delts."),
-    ("Push Press", "Shoulders", "Like a Military Press, but with momentum. Works the front delts."),
-    ("Overhead Dumbbell Press", "Shoulders", "Like a Military Press but with dumbbells. Works the front delts."),
-    ("Lateral Raise", "Shoulders", "Gripping dumbbels with your hands at the side of your body, raise them until they're above your clavicle line and lower. Works the side delts."),
+	("Military Press", "Shoulders", "With a barbell on your clavicles, push it up above your head with your arms. Works the front delts."),
+	("Push Press", "Shoulders", "Like a Military Press, but with momentum. Works the front delts."),
+	("Overhead Dumbbell Press", "Shoulders", "Like a Military Press but with dumbbells. Works the front delts."),
+	("Lateral Raise", "Shoulders", "Gripping dumbbels with your hands at the side of your body, raise them until they're above your clavicle line and lower. Works the side delts."),
 	("Lu Raise", "Shoulders", "Gripping weight with your hands at the side of your body, raise unntil they're above your head. Works the side delts and traps."),
 	("Front Raise", "Shoulders", "Holding a barbell or dumbbels in front of you, raise them until they're at eye level. Works the front delts."),
 	("Reverse Fly", "Shoulders", "Bent over with dumbbels on your hands, raise them sideways."),
