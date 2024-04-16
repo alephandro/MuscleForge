724322f5218d689b3d33f1954ef59e64a3aea966
GuilledlC <guillermodelacalhacar@gmail.com>
2024-04-16 16:41:29 +0200

    Add exercises table to Schema

--- Server/src/database.sql
+++ Server/src/database.sql
@@ -2,7 +2,13 @@ CREATE SCHEMA MuscleForge;
 
 USE MuscleForge;
 
-CREATE TABLE usuarios (
-                          email varchar(100),
-                          password varchar(100)
+CREATE TABLE users (
+	email varchar(100) PRIMARY KEY,
+	password varchar(100)
 );
+
+CREATE TABLE exercises (
+	name varchar(50) PRIMARY KEY,
+	muscle_group varchar(50)
+);
+CREATE UNIQUE INDEX idx_muscle_group ON exercises (muscle_group);
