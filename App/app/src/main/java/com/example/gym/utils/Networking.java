306117f53a93cf27b6a8d1e7ec8f13e8e416f21b
Solrac1388 <clsatocha@gmail.com>
2024-04-18 18:29:36 +0200

    Cambio IP

--- App/app/src/main/java/com/example/gym/utils/Networking.java
+++ App/app/src/main/java/com/example/gym/utils/Networking.java
@@ -2,7 +2,7 @@ package com.example.gym.utils;
 
 public class Networking {
 
-    public static final String IP = "10.0.2.2";
+    public static final String IP = "34.175.88.56";
 
     public static final int Port = 8888;
 
