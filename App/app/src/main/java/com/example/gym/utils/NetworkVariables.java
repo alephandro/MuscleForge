5ad43eafbdba15bfc4de611435ef508c12b7983f
alephandro <92859754+alephandro@users.noreply.github.com>
2024-04-22 17:27:23 +0200

    Update NetworkVariables.java

--- App/app/src/main/java/com/example/gym/utils/NetworkVariables.java
+++ App/app/src/main/java/com/example/gym/utils/NetworkVariables.java
@@ -2,7 +2,7 @@ package com.example.gym.utils;
 
 public class NetworkVariables {
 
-    public static final String IP = "34.175.88.56"; //10.0.2.2 for localhost
+    public static final String IP = "34.175.245.224"; //10.0.2.2 for localhost
 
     public static final int Port = 8888;
 
