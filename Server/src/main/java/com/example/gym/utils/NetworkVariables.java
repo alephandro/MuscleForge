2317b3246ea525524a886d911156878a93e97f61
alephandro <92859754+alephandro@users.noreply.github.com>
2024-04-23 23:00:27 +0200

    Update NetworkVariables.java

--- Server/src/main/java/com/example/gym/utils/NetworkVariables.java
+++ Server/src/main/java/com/example/gym/utils/NetworkVariables.java
@@ -2,7 +2,7 @@ package com.example.gym.utils;
 
 public class NetworkVariables {
 
-	public static final String BackupIP = "88.27.144.170";
+	public static final String BackupIP = "2.153.80.40";
 
 	public static final int BackupPort = 8888;
 
