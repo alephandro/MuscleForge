cafaf0c4fc3c20d896b2f3b4ca8549ae418aaac0
Solrac1388 <clsatocha@gmail.com>
2024-04-24 12:56:58 +0200

    Cambio de IP a url en NetworkVariables.java

--- Backup/src/main/java/utils/NetworkVariables.java
+++ Backup/src/main/java/utils/NetworkVariables.java
@@ -2,7 +2,7 @@ package utils;
 
 public class NetworkVariables {
 
-	public static final String ServerIP = "/34.175.245.224";
+	public static final String ServerIP = "/muscleforge-sever.duckdns.org";
 	public static final int ServerPort = 8888;
 
 }
