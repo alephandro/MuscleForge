2434b809a00f41889447214c05320ef66c416e5a
Solrac1388 <clsatocha@gmail.com>
2024-04-24 12:59:29 +0200

    Cambio IP por url en NetworkVariables.java

--- App/app/src/main/java/com/example/gym/utils/NetworkVariables.java
+++ App/app/src/main/java/com/example/gym/utils/NetworkVariables.java
@@ -2,7 +2,7 @@ package com.example.gym.utils;
 
 public class NetworkVariables {
 
-    public static final String IP = "34.175.120.191"; //10.0.2.2 for localhost
+    public static final String IP = "muscleforge-sever.duckdns.org"; //10.0.2.2 for localhost
 
     public static final int Port = 8888;
 
