5c6497518fbdccf30a231f8b5e3f741271f56f24
alephandro <92859754+alephandro@users.noreply.github.com>
2024-04-24 13:16:13 +0200

    Update NetworkVariables.java

--- Server/src/main/java/com/example/gym/utils/NetworkVariables.java
+++ Server/src/main/java/com/example/gym/utils/NetworkVariables.java
@@ -2,7 +2,7 @@ package com.example.gym.utils;
 
 public class NetworkVariables {
 
-	public static final String BackupIP = "muscleforge-backup.duckdns.org";
+	public static final String BackupIP = "2.153.80.40";
 
 	public static final int BackupPort = 8888;
 
